164bc66c010753e9515997ab924c2139e59435be
Zachary <251620301@qq.com>
2018-12-18 18:24:14 +0800

    init

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -1,8 +1,38 @@
 package com.example.demo.domain;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1l;
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(nullable = false,unique = true)
     private String name;
+    @Column(nullable = false)
     private String word;
+    @Column(nullable = false,unique = true)
+    private String email;
+    @Column(nullable = true,unique = true)
+    private String nickName;
+    @Column(nullable = false)
+    private String regTime;
+
+    public User() {
+    }
+
+    public User(String name, String word, String email, String nickName, String regTime) {
+        this.name = name;
+        this.word = word;
+        this.email = email;
+        this.nickName = nickName;
+        this.regTime = regTime;
+    }
 
     public String getName() {
         return name;
@@ -18,5 +48,41 @@ public class User {
 
     public void setWord(String word) {
         this.word = word;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getRegTime() {
+        return regTime;
+    }
+
+    public void setRegTime(String regTime) {
+        this.regTime = regTime;
     }
 }
